ddc6a22e8e570f44cc0d8043329804d6e7c65e7c
Anis <anis2k20@gmail.com>
2023-10-11 12:52:10 +0600

    Added api key, weather check function, and complete all work.

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import requests
+PINK = "#ffffff"
+
+api_key="2077c6d11f21406480a51937231110"
+base_url="http://api.weatherapi.com/v1/current.json"
+weather_params={
+    "key":api_key,
+    "q":"Paris",
+}
+
+def check_weather():
+    city=input_field.get()
+    weather_params["q"]=city
+    response = requests.get(base_url, params=weather_params)
+    data = response.json()
+
+
+    country = data["location"]['country']
+    time = data["location"]['localtime']
+    temp = data["current"]["temp_c"]
+    condition =data["current"]["condition"]["text"]
+    is_cloud = data["current"]["cloud"]
+    humidity = data["current"]["humidity"]
+    weather_report = f"Country: {country}\nTime: {time}\nTemperature: {temp}°C\nCondition: {condition}\nHumidity: {humidity}"
+    output.config(text=weather_report, font=("Arial",14,"normal"),bg=PINK,padx=5,pady=5)
+
 
 window = Tk()
 window.title("Weather App")
@@ -17,17 +43,18 @@ city.configure(pady=10)
 city.grid(column=1, row=1)
 input_field = Entry(width=35)
 input_field.grid(column=1, row=2,ipady=5)
+input_field.focus()
 
-btn = Button(text="Check Weather")
+btn = Button(text="Check Weather", command=check_weather)
 btn.grid(column=1,row=3,pady=10)
 
 weather_label = Label(text="The weather is: ",font=("Arial", 15, "bold"))
 weather_label.grid(column=1,row=4)
 
-
-weather_result = Entry(window,width=35)
-weather_result.place(width=200,height=100)
-weather_result.grid(column=1,row=5,pady=10,ipady=10)
+result=Canvas(width=200, height=100,bg=PINK)
+result.grid(column=1,row=5,padx=10,pady=10)
+output=Label(text="")
+output.grid(column=1,row=5)
 
 
 window.mainloop()
